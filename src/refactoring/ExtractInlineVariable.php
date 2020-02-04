a2ef7d3177413357b9d6908357431e32740a3fda
Victor Rentea <victorrentea@gmail.com>
2020-02-05 00:46:22 +0200

    + inline

--- src/refactoring/ExtractInlineVariable.php
+++ src/refactoring/ExtractInlineVariable.php
@@ -8,9 +8,10 @@ class ExtractInlineVariable
 {
     function computePrice(Order $order): float
     {
-        return $order->quantity * $order->itemPrice -
+        $result = $order->quantity * $order->itemPrice -
             max(0, $order->quantity - 500) * $order->itemPrice * 0.05 +
             min($order->quantity * $order->itemPrice * 0.1, 100);
+        return $result;
     }
 }
 
