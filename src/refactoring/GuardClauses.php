48d1d29771948d0c0f7e87824cf5211ff8ca3a1c
Victor Rentea <victorrentea@gmail.com>
2020-02-05 02:18:57 +0200

    Guard Clauses

---
+++ src/refactoring/GuardClauses.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace victor\refactoring;
+
+
+class GuardClauses
+{
+    private $isDead = false;
+    private $isSeparated = false;
+    private $isRetired = false;
+
+    function getPayAmount()
+    {
+        if ($this->isDead)
+            $result = $this->deadAmount();
+        else {
+            if ($this->isSeparated)
+                $result = $this->separatedAmount();
+            else {
+                if ($this->isRetired)
+                    $result = $this->retiredAmount();
+                else
+                    $result = $this->normalPayAmount();
+            }
+        }
+        return $result;
+    }
+
+    private function deadAmount()
+    {
+        return 1;
+    }
+
+    private function retiredAmount()
+    {
+        return 2;
+    }
+
+    private function separatedAmount()
+    {
+        return 3;
+    }
+
+    private function normalPayAmount()
+    {
+        return 4;
+    }
+
+}
