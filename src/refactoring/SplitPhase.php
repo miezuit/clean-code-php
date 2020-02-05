1378a3e1f317cbb8512e13506010b950b4284396
Victor Rentea <victorrentea@gmail.com>
2020-02-06 01:31:56 +0200

    yolo

--- src/refactoring/SplitPhase.php
+++ src/refactoring/SplitPhase.php
@@ -7,6 +7,7 @@ namespace victor\refactoring;
 class SplitPhase
 {
 
+    // TODO: pass individual pieces
     // Example 1
     function calculateOrderPrice(string $orderString, array $priceList): float {
         $orderData = preg_split('/\s+/', $orderString);
