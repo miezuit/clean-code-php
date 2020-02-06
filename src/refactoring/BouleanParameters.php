a7b795854eb6fbb72173097c31d72bb6286b21f5
Victor Rentea <victorrentea@gmail.com>
2020-02-06 15:57:43 +0200

    fighting the boolean

--- src/refactoring/BouleanParameters.php
+++ src/refactoring/BouleanParameters.php
@@ -4,54 +4,87 @@
 namespace victor\refactoring;
 
 $boule = new BouleanParameters();
-$boule->bigUglyMethod(1, 2, false);
-$boule->bigUglyMethod(1, 2, false);
-$boule->bigUglyMethod(1, 2, false);
-$boule->bigUglyMethod(1, 2, false);
-$boule->bigUglyMethod(1, 2, false);
+$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethod(1, 2);
+$boule->bigUglyMethod(1, 2);
 
 // TODO GIGEL:  From my use-case, I call it too, to do more within:
-$boule->bigUglyMethod(1, 2, true);
+$boule->bigUglyMethodGigel(1, 2);
+
 
 class BouleanParameters
 {
 
-    function bigUglyMethod(int $a, int $b, bool $gigel)
+    function bigUglyMethod(int $a, int $b)
+    {
+        $this->beforeLogic();
+        $this->afterLogic();
+    }
+
+    public function beforeLogic(): int
     {
         echo "Complex Logic\n";
         echo "Complex Logic\n";
         echo "Complex Logic\n";
-
-        if ($gigel) {
-            echo "Gigel: pun si eu aicea logica de care am eu nevoie";
-        }
-
-        echo "More Complex Logic\n";
-        echo "More Complex Logic\n";
-        echo "More Complex Logic\n";
+        return 1;
     }
 
 
     // ============== "BOSS" LEVEL: A lot harder to break down =================
 
-    function bossLevel(bool $stuff, bool $fluff, array $tasks)
+    public function afterLogic(): void
+    {
+        echo "More Complex Logic\n";
+        echo "More Complex Logic\n";
+        echo "More Complex Logic\n";
+    }
+
+    function bigUglyMethodGigel(int $a, int $b)
+    {
+        $this->beforeLogic();
+        $this->gigelMethod();
+        $this->afterLogic();
+    }
+
+    public function gigelMethod(): void
+    {
+        echo "Gigel: pun si eu aicea logica de care am eu nevoie";
+        echo "Gigel: pun si eu aicea logica de care am eu nevoie";
+        echo "Gigel: pun si eu aicea logica de care am eu nevoie";
+    }
+
+    function bossLevelStuffFluff(array $tasks)
     {
         $i = 0;
         $j = 1;
         echo "Logic1\n";
-        if ($stuff) {
-            echo "Logic2\n";
-            if ($fluff) {
-                echo "Logic3\n";
-                foreach ($tasks as $task) {
-                    $i++;
-                    echo "Logic4 $task\n";
-                    // TODO HERE, when call this method, I want MY own custom code to run here
-                    echo "Logic5 $i\n";
-                }
-                echo "Logic6 " . $j++ . "\n";
-            }
+        echo "Logic2\n";
+        echo "Logic3\n";
+        foreach ($tasks as $task) {
+            $i++;
+            echo "Logic4 $task\n";
+            // TODO HERE, when call this method, I want MY own custom code to run here
+            echo "Logic5 $i\n";
         }
+        echo "Logic6 " . $j++ . "\n";
+        echo "Logic7 $j\n";
+    }
+    function bossLevelStuffNoFluff(array $tasks)
+    {
+        $i = 0;
+        $j = 1;
+        echo "Logic1\n";
+        echo "Logic2\n";
+        echo "Logic7 $j\n";
+    }
+
+    function bossLevelNoStuff(bool $fluff, array $tasks)
+    {
+        $i = 0;
+        $j = 1;
+        echo "Logic1\n";
         echo "Logic7 $j\n";
     }
 }
