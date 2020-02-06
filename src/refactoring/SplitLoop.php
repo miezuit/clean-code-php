10398f54a7ed9f40d50ef819f3680839107990b0
Victor Rentea <victorrentea@gmail.com>
2020-02-06 11:44:45 +0200

    SplitLoop

--- src/refactoring/SplitLoop.php
+++ src/refactoring/SplitLoop.php
@@ -6,15 +6,32 @@ namespace victor\refactoring;
 class SplitLoop {
     /** @param Employee[] $employees */
     static function computeStats(array $employees) {
-        $averageAge = 0;
+        $averageSalary = self::computeAverageSalary($employees);
+        $averageAge = self::computeAverageAge($employees);
+        echo "avg age = $averageAge\navg sal = $averageSalary\n";
+    }
+
+    public static function computeAverageSalary(array $employees): float
+    {
         $totalSalary = 0;
+        foreach ($employees as $employee) {
+            $totalSalary += $employee->salary;
+        }
+        return $totalSalary / count($employees);
+    }
+
+    /**
+     * @param array $employees
+     * @return float|int
+     */
+    public static function computeAverageAge(array $employees)
+    {
+        $averageAge = 0;
         foreach ($employees as $e) {
             $averageAge += $e->age;
-            $totalSalary += $e->salary;
         }
         $averageAge = $averageAge / sizeof($employees);
-        $averageSalary = $totalSalary / sizeof($employees);
-        echo "avg age = $averageAge\navg sal = $averageSalary\n";
+        return $averageAge;
     }
 }
 
