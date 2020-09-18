c5fd52aa15a691d650c3a26bdde6058acc6b6ef4
Radu Gasler <radu.gasler@2checkout.com>
2020-09-18 12:39:18 +0300

    Extracted smaller methods to simplify a complex method

--- src/refactoring/videostore/Customer.php
+++ src/refactoring/videostore/Customer.php
@@ -33,36 +33,19 @@ class Customer
         $rentals = $this->rentals;
         $result = 'Rental Record for ' . $this->getName() . "\n";
 
-        foreach ($rentals as $each) {
-            $thisAmount = 0;
-
-            // determines the amount for each line
-            switch ($each->getMovie()->getPriceCode()) {
-                case Movie::PRICE_CODE_REGULAR:
-                    $thisAmount += 2;
-                    if ($each->getDaysRented() > 2)
-                        $thisAmount += ($each->getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie::PRICE_CODE_NEW_RELEASE:
-                    $thisAmount += $each->getDaysRented() * 3;
-                    break;
-                case Movie::PRICE_CODE_CHILDREN:
-                    $thisAmount += 1.5;
-                    if ($each->getDaysRented() > 3)
-                        $thisAmount += ($each->getDaysRented() - 3) * 1.5;
-                    break;
-            }
+        foreach ($rentals as $rental) {
+            $thisAmount = $this->determineTheAmountForEachLine($rental);
 
             // add frequent renter points
             $frequentRenterPoints++;
 
             // add bonus for a two day new release rental
-            if ($each->getMovie()->getPriceCode() == Movie::PRICE_CODE_NEW_RELEASE
-                && $each->getDaysRented() > 1)
+            if ($this->shouldApplyBonus($rental)) {
                 $frequentRenterPoints++;
+            }
 
             // add footer lines
-            $result .= "\t" . $each->getMovie()->getTitle() . "\t"
+            $result .= "\t" . $rental->getMovie()->getTitle() . "\t"
                 . $thisAmount . "\n";
             $totalAmount += $thisAmount;
 
@@ -73,5 +56,42 @@ class Customer
 
 
         return $result;
+    }
+
+    /**
+     * @param Rental $each
+     * @return float|int
+     */
+    protected function determineTheAmountForEachLine(Rental $each)
+    {
+        $thisAmount = 0;
+
+        // determines the amount for each line
+        switch ($each->getMovie()->getPriceCode()) {
+            case Movie::PRICE_CODE_REGULAR:
+                $thisAmount += 2;
+                if ($each->getDaysRented() > 2)
+                    $thisAmount += ($each->getDaysRented() - 2) * 1.5;
+                break;
+            case Movie::PRICE_CODE_NEW_RELEASE:
+                $thisAmount += $each->getDaysRented() * 3;
+                break;
+            case Movie::PRICE_CODE_CHILDREN:
+                $thisAmount += 1.5;
+                if ($each->getDaysRented() > 3)
+                    $thisAmount += ($each->getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return $thisAmount;
+    }
+
+    /**
+     * @param Rental $rental
+     * @return bool
+     */
+    protected function shouldApplyBonus(Rental $rental): bool
+    {
+        return $rental->getMovie()->getPriceCode() == Movie::PRICE_CODE_NEW_RELEASE
+            && $rental->getDaysRented() > 1;
     }
 }
